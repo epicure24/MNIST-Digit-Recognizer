f0d22c9c329ff920159ec167df30434b5a33255d
Shweta Sharma <sshweta2407@gmail.com>
2020-09-27 15:56:58 +0530

    LeNet5 Architecture Model

---
+++ LeNet5/model_architecture.py
@@ -0,0 +1,32 @@
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D, BatchNormalization
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import LearningRateScheduler
+from keras.callbacks import ReduceLROnPlateau
+
+model = Sequential()
+
+model.add(Conv2D(32, kernel_size = 3, activation='relu', input_shape = (28, 28, 1)))
+model.add(BatchNormalization())
+model.add(Conv2D(32, kernel_size = 3, activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(32, kernel_size = 5, strides=2, padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+
+model.add(Conv2D(64, kernel_size = 3, activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(64, kernel_size = 3, activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(64, kernel_size = 5, strides=2, padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+
+model.add(Conv2D(128, kernel_size = 4, activation='relu'))
+model.add(BatchNormalization())
+model.add(Flatten())
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+
+
+model.summary()
